e92990c22667951aae4f714a436ca7b7267c66dc
Myeongjin Kim <myeongjin1115@gmail.com>
2021-06-04 20:12:30 +0900

    update main, header, changeLP, term_reg_func

--- header.php
+++ header.php
@@ -3,7 +3,7 @@ if(!isset($_SESSION['userid'])){
 echo('	
 <center>
 	<form action="login.php" method="POST">
-		<div id="title"><font size="6"> 강릉원주대 학사정보 시스템 </font></div>
+		<div id="title"><font size="6"><a href="/main.html"> 강릉원주대 학사정보 시스템 </a></font></div>
 		<div id="log">
 			<font size="2">
 				ID : <input type="text" size="12" name="ID" required>&nbsp&nbsp
@@ -18,7 +18,7 @@ echo('
 	echo('
 <center>
 	<form action="logout.php" method="POST">
-		<div id="title"><font size="6"> 강릉원주대 학사정보 시스템 </font></div>
+		<div id="title"><font size="6"><a href="/main.html"> 강릉원주대 학사정보 시스템 </a></font></div>
 		<div id="log"><font size="2"> "'.$_SESSION["username"].'"님 안녕하세요.</font></div>
 		<div id="logsubmit"><input type="submit" value="로그아웃"></div>
 	</form>
